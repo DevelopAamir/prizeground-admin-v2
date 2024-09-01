793d76a32737a1246dc554df7883314d5637f863
maazahmar <maazahmar@gmail.com>
2024-09-02 05:23:40 +0545

    settings arranged

--- prizeground/settings.py
+++ prizeground/settings.py
@@ -131,7 +131,10 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 MEDIA_URL="/media/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 
 
